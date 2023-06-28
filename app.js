afcad37d609d6272cb58abe4268b27b81c758e67
AndrewIskander <121323915+AndrewIskander@users.noreply.github.com>
2023-06-28 21:04:18 +0300

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/hello/:name', (req, res) => res.send('Hello, ' + req.params.name))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
